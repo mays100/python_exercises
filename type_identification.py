800a58ada0c3c86f6054cf61b7c415136bcf5fa9
may sabag <maysbg321@gmail.com>
2025-04-23 09:51:54 +0300

    הוספת פתרון לתרגיל type() function

---
+++ type_identification.py
@@ -0,0 +1,7 @@
+num = 10
+str_var = "Hello"
+bool_var = True
+
+print(f"הערך של num הוא {num} והסוג שלו הוא {type(num)}")
+print(f"הערך של str הוא '{str_var}' והסוג שלו הוא {type(str_var)}")
+print(f"הערך של bool הוא {bool_var} והסוג שלו הוא {type(bool_var)}")
